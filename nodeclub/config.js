be9cc7c536e537a029f3d87163e6b6adef942b6a
Jack.Zhang <zhangmj@eicodesign.com>
2014-09-04 14:07:48 +0800

    noclub更新config.js，添加github token

--- nodeclub/config.js
+++ nodeclub/config.js
@@ -84,9 +84,9 @@ var config = {
 
   // github 登陆的配置
   GITHUB_OAUTH: {
-    clientID: 'your GITHUB_CLIENT_ID',
-    clientSecret: 'your GITHUB_CLIENT_SECRET',
-    callbackURL: 'http://cnodejs.org/auth/github/callback',
+    clientID: '0d1d0dab43184816671f',
+    clientSecret: '6469ac905aae2fae5a3d30124c556fd75626a807',
+    callbackURL: 'http://club.zhangskills.xicp.net/auth/github/callback',
   },
   // 是否允许直接注册（否则只能走 github 的方式）
   allow_sign_up: true,
